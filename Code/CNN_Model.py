5a8dceaedc2405e9e266674fe9937a1c3cdab3d5
Niktennant <98777012+Niktennant@users.noreply.github.com>
2023-05-08 00:05:24 -0400

    update

--- Code/CNN_Model.py
+++ Code/CNN_Model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74fd997d7a93c03edf3937f6681c24ee68d82ba320b5a803e98db933ec57f80d
-size 10047
+oid sha256:c0a88605bc14b808c5dabb2f0381ec01eb51f210c6ebe300ad6eb2a0631640fb
+size 9905
--- Code/main.py
+++ Code/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ceac6bcfd7df28b6bc2259708ede4fd86f44230d1e254764a63160ada8c353e
-size 10824
+oid sha256:803905abe7e023ef8033f7cb35cfdad60e5d22392eefcf57938a04cb0b10c526
+size 10899
