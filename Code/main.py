4e95f96f1fe1334ecea9cde9bfd2df630ddf762b
Niktennant <98777012+Niktennant@users.noreply.github.com>
2023-05-12 03:49:46 -0400

    figures updated

--- Code/main.py
+++ Code/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:904a70be36372628950dd4e579f9612f0cda04ff917a3fc911bf04e775ef2ae2
-size 11287
+oid sha256:7c32f6967885b16d21aeee2aec5262b69a66edfd71c29f6d580db09f9d0eb571
+size 11290
